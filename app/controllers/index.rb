8297523f63fb8daf10bebe196278c4f96f86bdf4
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-21 18:06:02 -0500

    first commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,22 @@
+get '/' do
+  @events = Event.all
+  erb :index
+end
+
+get '/events/:id/show' do |id|
+  @event = Event.find(id)
+  erb :event_show
+end
+
+get '/events/new' do
+  erb :new_event
+end
+
+post '/events/create' do
+  @event = Event.create(params[:event])
+  if @event.errors.any?
+    redirect to('/events/new')
+  else
+    redirect to('/')
+  end
+end
